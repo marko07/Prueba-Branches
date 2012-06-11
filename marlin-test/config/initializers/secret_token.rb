e79d45e932741b4dc34e3a0fef02cd17e1448dba
Marco Pacheco <marcopache@gmail.com>
2012-06-11 10:27:05 -0600

    devise active admin rails 2.3.5 ruby 193

---
+++ marlin-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarlinTest::Application.config.secret_token = '0cc12811f480ee73be26fb657a9f91a4dc94e50f0f7ff529ae97d89a199c328b42042626a76fa9188480ea0d6599f524813e3d8fc1021861143487b2f2ebd050'
